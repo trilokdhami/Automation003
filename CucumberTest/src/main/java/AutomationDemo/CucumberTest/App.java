8c8298a54ec4f5f9b4919e8987b56c8539876ea7
dhamitrilok <dhamitrilok7@gmail.com>
2022-05-01 18:45:20 +0530

    Added a new rar file

---
+++ CucumberTest/src/main/java/AutomationDemo/CucumberTest/App.java
@@ -0,0 +1,13 @@
+package AutomationDemo.CucumberTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ CucumberTest/src/test/java/features/Login.feature
@@ -0,0 +1,9 @@
+Feature: Application Login 
+
+
+Scenario: Homepage default page
+Given  user is on landing page 
+When user login in application 
+Then homepage is popilated
+And all the cards are displayed.
+
